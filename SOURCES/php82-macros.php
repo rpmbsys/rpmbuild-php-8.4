40169f719e0924ee6feb703f9dcca8a2b579ad6a
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-18 23:57:33 +0100

    Corrected paths for PHP 8.2

--- SOURCES/php82-macros.php
+++ SOURCES/php82-macros.php
@@ -6,12 +6,12 @@
 %php_pdo_api  @PHP_PDOVER@
 %php_version  @PHP_VERSION@
 
-%php_extdir    %{_libdir}/php81/modules
+%php_extdir    %{_libdir}/php82/modules
 
-%php_inidir    %{_sysconfdir}/php81/php.d
+%php_inidir    %{_sysconfdir}/php82/php.d
 
-%php_incldir    %{_includedir}/php81
+%php_incldir    %{_includedir}/php82
 
-%__php         %{_bindir}/php81
+%__php         %{_bindir}/php82
 
-%pecl_xmldir   %{_sharedstatedir}/php81/peclxml
+%pecl_xmldir   %{_sharedstatedir}/php82/peclxml
